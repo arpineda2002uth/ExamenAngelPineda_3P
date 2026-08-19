a9a26f318389d78e3287a597b5d90113f446cc6a
arpineda2002uth <arpineda2002@gmail.com>
2026-08-18 19:25:59 -0600

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("Clasificador de Imágenes - Ángel Pineda")
 
 # Cargar el modelo entrenado en formato SavedModel
 # Asegúrate de subir la carpeta "modelo_cifar10" completa a tu repositorio
-model = tf.keras.models.load_model("modelo_cifar10")
+model = tf.keras.models.load_model("modelo_cifar10.h5")
 
 # Etiquetas del dataset CIFAR-10
 labels = ['avión','auto','pájaro','gato','ciervo','perro','rana','caballo','barco','camión']
